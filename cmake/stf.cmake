d7852b070fdbe7e99d46c0d6ab4a6054cae8b12a
jjxia81 <xiajianjun2011@hotmail.com>
2026-04-25 20:15:50 -0500

    use modified stf version

--- cmake/stf.cmake
+++ cmake/stf.cmake
@@ -1,3 +1,15 @@
+# if (TARGET stf::stf)
+#     return()
+# endif()
+
+# include(CPM)
+# CPMAddPackage(
+#     NAME stf
+#     GITHUB_REPOSITORY adobe-research/space-time-functions
+#     GIT_TAG b4c4a7e54400a2fa0389b2b738301e472c06282e
+# )
+
+
 if (TARGET stf::stf)
     return()
 endif()
@@ -5,6 +17,6 @@ endif()
 include(CPM)
 CPMAddPackage(
     NAME stf
-    GITHUB_REPOSITORY adobe-research/space-time-functions
-    GIT_TAG b4c4a7e54400a2fa0389b2b738301e472c06282e
+    GITHUB_REPOSITORY jjxia81/space-time-functions
+    GIT_TAG main
 )

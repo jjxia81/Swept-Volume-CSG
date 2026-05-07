df98f3d7e1f409dfd416a4f3c0b5ea525824465c
xingyidu <du.xingyi@wustl.edu>
2026-05-07 15:56:56 -0700

    Update cell-complex orphan cleanup pin

--- cmake/cellcomplex.cmake
+++ cmake/cellcomplex.cmake
@@ -6,5 +6,5 @@ include(CPM)
 CPMAddPackage(
     NAME cellcomplex
     GITHUB_REPOSITORY duxingyi-charles/cell-complex
-    GIT_TAG 5841acb9ccf9922421529d620e98d7e6dafdb367
+    GIT_TAG fa1f4772928f2f8a5c0f8eace86a9e625b1a697f
 )
